8f227ed43b77301a3a3e41669fa6eeb12aff023d
Aleksandar <laraluda.g@gmail.com>
2021-01-23 22:01:37 +0100

    changed names of routes added routes added story

--- text_based_adventure_game/game/urls.py
+++ text_based_adventure_game/game/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('game', views.game, name="game"),
+    path('intro', views.intro, name="intro"),
+    path('choose_path', views.choose_path, name="choose_path"),
+    path('path_mountain', views.path_mountain, name="path_mountain"),
+    path('path_mountain/path_mountain_first_choice', views.path_mountain_first_choice, name="path_mountain_first_choice"),
+    path('path_slow_river', views.path_mountain, name="path_slow_river"),
 ]
--- text_based_adventure_game/game/views.py
+++ text_based_adventure_game/game/views.py
@@ -5,5 +5,21 @@ def index(request):
     return render(request, 'index.html')
 
 
-def game(request):
-    return render(request, 'game.html')
+def intro(request):
+    return render(request, 'intro.html')
+
+
+def choose_path(request):
+    return render(request, 'choose_path.html')
+
+
+def path_mountain(request):
+    return render(request, 'path_mountain.html')
+
+
+def path_mountain_first_choice(request):
+    return render(request, 'path_mountain_first_choice.html')
+
+
+def path_slow_river(request):
+    return render(request, 'path_slow_river.html')
